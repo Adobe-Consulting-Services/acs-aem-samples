9bc20cf9e5c1e6bb8a676ff3154987f5c6605af2
david g <davidjgonzalez@gmail.com>
2016-11-21 14:27:57 -0500

    Enhancements to SlingModel example

--- bundle/src/main/java/com/adobe/acs/samples/models/SampleSlingModel.java
+++ bundle/src/main/java/com/adobe/acs/samples/models/SampleSlingModel.java
@@ -23,17 +23,22 @@ package com.adobe.acs.samples.models;
 import com.day.cq.search.PredicateGroup;
 import com.day.cq.search.Query;
 import com.day.cq.search.QueryBuilder;
+import org.apache.commons.lang.StringUtils;
 import org.apache.sling.api.resource.Resource;
 import org.apache.sling.api.resource.ResourceResolver;
 import org.apache.sling.models.annotations.Default;
+import org.apache.sling.models.annotations.DefaultInjectionStrategy;
 import org.apache.sling.models.annotations.Model;
 import org.apache.sling.models.annotations.Optional;
+import org.apache.sling.models.annotations.Required;
 import org.apache.sling.models.annotations.Source;
+import org.apache.sling.models.annotations.injectorspecific.Self;
 
 import javax.annotation.PostConstruct;
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.jcr.Session;
+import java.util.Calendar;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -54,22 +59,42 @@ import java.util.Map;
  *  > http://sling.apache.org/documentation/bundles/models.html#available-injectors
  *  > Ensure you are using the latest Sling Model API and Impl bundles for access
  *    to the latest and greatest Injectors
- *
  */
-@Model(adaptables=Resource.class)
+
+/** THIS SAMPLE TARGETS SLING MODELS v1.3+ **/
+
+@Model(
+        adaptables = Resource.class,
+        defaultInjectionStrategy = DefaultInjectionStrategy.OPTIONAL
+)
 public class SampleSlingModel {
 
-    // Inject a property whose name does NOT match the Model field name
-    @Inject @Named("jcr:title")
+    @Self
+    private Resource resource;
+
+    // Inject a property name whose name does NOT match the Model field name
+    // Since the Default inject strategy is OPTIONAL (set on the @Model), we can mark injections as @Required. @Optional can be used if the default strategy is REQUIRED.
+    @Inject @Named("jcr:title") @Required
     private String title;
 
-    // Inject a property whose name DOES match the Model field name
-    @Inject @Default(values = "No description provided")
+    // Inject a fields whose property name DOES match the model field name
+    @Inject @Optional
+    private String pageTitle;
+
+    // Mark as Optional
+    @Inject @Optional
+    private String navTitle;
+
+    // Provide a default value if the property name does not exist
+    @Inject @Named("jcr:description") @Default(values = "No description provided")
     private String description;
 
-    // Set a default value
-    @Inject @Optional
-    private String theme;
+    // Various data types can be injected
+    @Inject @Named("jcr:created")
+    private Calendar createdAt;
+
+    @Inject
+    boolean navRoot;
 
     // Inject OSGi services
     @Inject
@@ -81,31 +106,68 @@ public class SampleSlingModel {
     @Inject @Source("sling-object")
     private ResourceResolver resourceResolver;
 
-    // Some other internal Model state
-    private long totalMatches;
+    // Internal state populated via @PostConstruct logic
+    private long descendantPagesCount;
 
     @PostConstruct
     // PostConstructs are called after all the injection has occurred, but before the Model object is returned for use.
-    private void calculateTotalMatches() {
+    private void initialize() {
         // Note that @PostConstruct code will always be executed on Model instantiation.
-        // If the work done in PostConstract is expensive and not always used in the consumption of the model, it is
+        // If the work done in PostConstruct is expensive and not always used in the consumption of the model, it is
         // better to lazy-execute the logic in the getter and persist the result in  model state if it is requested again.
+
         final Map<String, String> map = new HashMap<String, String>();
-        map.put("path", "/content");
+        // Injected fields can be used to define logic
+        map.put("path", resource.getParent().getPath());
         map.put("type", "cq:Page");
 
         Query query = queryBuilder.createQuery(PredicateGroup.create(map), resourceResolver.adaptTo(Session.class));
         final com.day.cq.search.result.SearchResult result = query.getResult();
-        this.totalMatches = result.getTotalMatches();
+        this.descendantPagesCount = result.getTotalMatches();
     }
 
-    // This getter exposes data Injected into the Model
+    /**
+     * This getter wraps business logic around how an logic data point (title) is represented for this resource.
+     *
+     * @return The Page Title if exists, with fallback to the jcr:title
+     */
+    public String getTitle() {
+        return StringUtils.defaultIfEmpty(pageTitle, title);
+    }
+
+    /**
+     * This getter exposes data Injected into the Model and allows parameterized manipulation of the output.
+     *
+     * @param truncateAt length at which to truncate
+     * @return the truncated description.
+     */
     public String getDescription(int truncateAt) {
         return this.description.substring(0, truncateAt) + "...";
     }
 
-    // This getter exposes the work of a @PostConstruct method
-    public long getTotalMatches() {
-        return this.totalMatches;
+
+    /**
+     * Default implementation of the parameterizable getDescription(..).
+     *
+     * @return the truncated description.
+     */
+    public String getDescription() {
+        return this.getDescription(10);
+    }
+
+    /**
+     * This getter exposes the work of a @PostConstruct method.
+     *
+     * @return the number of cq:Pages that exist under this resource.
+     */
+    public long getDescendantPagesCount() {
+        return this.descendantPagesCount;
+    }
+
+    /**
+     * @return the created at Calendar value.
+     */
+    public Calendar getCreatedAt() {
+        return createdAt;
     }
 }
