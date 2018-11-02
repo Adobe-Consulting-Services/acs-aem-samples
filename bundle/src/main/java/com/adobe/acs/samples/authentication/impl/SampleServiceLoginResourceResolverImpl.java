14063227e1440a5ad31bbd66666ed67388101bb8
david g <davidjgonzalez@users.noreply.github.com>
2018-11-02 08:31:42 -0400

    update to use try-with-resources

--- bundle/src/main/java/com/adobe/acs/samples/authentication/impl/SampleServiceLoginResourceResolverImpl.java
+++ bundle/src/main/java/com/adobe/acs/samples/authentication/impl/SampleServiceLoginResourceResolverImpl.java
@@ -23,6 +23,7 @@ import java.util.Map;
 public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
     private static final Logger log = LoggerFactory.getLogger(SampleServiceLoginResourceResolverImpl.class);
 
+    
     // This "identifier" does NOT directly correlate to a CRX User; The mapping of this value to the CRX User 
     // happens in the ServiceUserMapper OSGi config
     //
@@ -33,7 +34,60 @@ public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
     @Reference
     private ResourceResolverFactory resourceResolverFactory;
 
+     /** try-with-resources and auto-closing ResourceResolver introduced in: https://issues.apache.org/jira/browse/SLING-4805 **/
+    
+     /**
+     * This uses the try-with-resources approach and auto-closes the resource resolver.
+     * Available in AEM 6.2+ and Java 7+.
+     * 
+     * Example for getting the Bundle Service User
+     *
+     * @return the user ID
+     */
+    private final String getBundleServiceUser() {
+        // Get the auto-closing Service resource resolver
+        try( ResourceResolver serviceResolver = resourceResolverFactory.getServiceResourceResolver(null)) {
+            serviceResolver = ;
+
+            if (serviceResolver != null) {
+                // Do some work w your service resource resolver
+                return serviceResolver.getUserID();
+            } else {
+                return "Could not obtain a User for Bundle Service";
+            }
+        }
+    }
+    
+     /**
+     * The try-catch-finally approach can be replaced with the try-with-resources approach defined above in AEM 6.2 and later and Java7+.
+     * 
+     * Example for getting the Bundle SubService User
+     *
+     * @return the user ID
+     */
+    private final String getSubServiceUser() {
+        // Create the Map to pass in the Service Account Identifier
+        // Remember, "SERVICE_ACCOUNT_IDENTIFIER" is mapped  to the CRX User via a SEPARATE ServiceUserMapper Factory OSGi Config
+        final Map<String, Object> authInfo = Collections.singletonMap(
+                    ResourceResolverFactory.SUBSERVICE,
+                    (Object) SERVICE_ACCOUNT_IDENTIFIER);
+
+        // Get the auto-closing Service resource resolver        
+        try (ResourceResolver serviceResolver = resourceResolverFactory.getServiceResourceResolver(authInfo)) {
+            if (serviceResolver != null) {
+                // Do some work w your service resource resolver
+                return serviceResolver.getUserID();
+            } else {
+                return "Could not obtain a User for the Service: " + SERVICE_ACCOUNT_IDENTIFIER;
+            }
+        } 
+    }
+    
+    /** LEGACY TRY-CATCH-FINALLY EXAMPLES ARE BELOW **/
+
     /**
+     * The try-catch-finally approach can be replaced with the try-with-resources approach defined above in AEM 6.2 and later and Java7+.
+     * 
      * Example for getting the Bundle Service User
      *
      * @return the user ID
@@ -42,7 +96,7 @@ public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
 
         ResourceResolver serviceResolver = null;
 
-        try {
+        try () {
             // Get the Service resource resolver
             serviceResolver = resourceResolverFactory.getServiceResourceResolver(null);
 
@@ -54,7 +108,7 @@ public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
                 return "Could not obtain a CRX User for Bundle Service";
             }
         } catch (LoginException e) {
-            return "Login Exception when obtaining a CRX User for the Bundle Service - " + e.getMessage();
+            return "Login Exception when obtaining a User for the Bundle Service - " + e.getMessage();
         } finally {
             // As always, clean up any ResourceResolvers or JCR Sessions you open
             if (serviceResolver != null) {
@@ -65,6 +119,8 @@ public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
 
 
     /**
+     * The try-catch-finally approach can be replaced with the try-with-resources approach defined above in AEM 6.2 and later and Java7+.
+     * 
      * Example for getting the Bundle SubService User
      *
      * @return the user ID
@@ -91,7 +147,7 @@ public class SampleServiceLoginResourceResolverImpl implements SampleExecutor {
                 return "Could not obtain a CRX User for the Service: " + SERVICE_ACCOUNT_IDENTIFIER;
             }
         } catch (LoginException e) {
-            return "Login Exception when obtaining a CRX User for the Service: " + SERVICE_ACCOUNT_IDENTIFIER + " - " + e.getMessage();
+            return "Login Exception when obtaining a User for the Service: " + SERVICE_ACCOUNT_IDENTIFIER + " - " + e.getMessage();
         } finally {
             // As always, clean up any ResourceResolvers or JCR Sessions you open
             if (serviceResolver != null) {
