7cee5997741f21c7a43d3e02236730971b6bc0d8
david g <davidjgonzalez@gmail.com>
2020-01-03 15:47:26 -0500

    Removed AngularJS Samples since we have SPA Editor now

--- ui.apps/src/main/content/jcr_root/apps/acs-aem-samples/angularjs-app/clientlibs/footer/js/angular/core/base-controller.js
+++
@@ -1,9 +0,0 @@
-/*global sampleModule*/
-
-/*
- * The baseController is being assigned in the page template on the body element.
- * It is most useful when needing to assign values to scope which will be shared between all pages and components in the application.
- */
-sampleModule.controller('baseController', ['$scope', function($scope) {
-
-}]);
--- ui.apps/src/main/content/jcr_root/apps/acs-aem-samples/angularjs-app/pages/base-page/content.jsp
+++
@@ -1,6 +0,0 @@
-<%@page session="false" %>
-<%@include file="/libs/foundation/global.jsp" %>
-
-<div id="bodyContent">
-	<cq:include path="mainContent" resourceType="foundation/components/parsys"/>
-</div>
